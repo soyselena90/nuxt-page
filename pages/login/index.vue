c094f1f505ac563a8159e3bdd9d42317c08530ab
SOYEON CHOI <soyselena90@naver.com>
2024-08-22 00:03:45 +0900

    feat : 로그인 화면 , 해더 텍스트 슬라이드 및 로그인 완료 시 로그 아웃

---
+++ pages/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import {definePageMeta} from "#imports";
+
+definePageMeta({
+    layout: "login"
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
